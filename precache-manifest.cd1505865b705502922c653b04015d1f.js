ec46856a5fca437845ccae450b8e2add7873b5c7
Dmitriy Serdtsev <in19farkt@gmail.com>
2019-08-14 15:30:59 +0300

    Updates

---
+++ precache-manifest.cd1505865b705502922c653b04015d1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d723757fa1136fe776ca18abcda831ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db862dc68e6e895df4c8",
+    "url": "/static/css/main.3a2591b4.chunk.css"
+  },
+  {
+    "revision": "d1e975fe7886f3effca3",
+    "url": "/static/js/2.e060fdbc.chunk.js"
+  },
+  {
+    "revision": "db862dc68e6e895df4c8",
+    "url": "/static/js/main.e779e00e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "888df99110b9c99e5deabc6c33ec26a2",
+    "url": "/static/media/Ethereum-logo.888df991.png"
+  }
+]);
